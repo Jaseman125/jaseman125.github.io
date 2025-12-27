3bd59684fbc0a588c7d421312d9108c5f0564924
Jaseman <jaseman125@googlemail.com>
2025-12-27 12:49:34 +0000

    400

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ function applyPhoneMode() {
     showFrame("IF03P");
     showFrame("IF04P");
 
-    setWidth("IF01P", "360px");
-    setWidth("IF02P", "360px");
-    setWidth("IF03P", "360px");
-    setWidth("IF04P", "360px");
+    setWidth("IF01P", "400px");
+    setWidth("IF02P", "400px");
+    setWidth("IF03P", "400px");
+    setWidth("IF04P", "400px");
 }
 
 function applyDesktopMode() {
