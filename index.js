6d63c060766b38537b9492d79f7a93702f0d87dc
Jaseman <jaseman125@googlemail.com>
2025-12-27 02:32:19 +0000

    phone mode added

---
+++ index.js
@@ -0,0 +1,50 @@
+function updateMode() {
+    const phone = document.body.clientWidth < 1521;
+
+    if (phone) {
+        applyPhoneMode();
+    } else {
+        applyDesktopMode();
+    }
+}
+
+window.addEventListener("load", updateMode);
+window.addEventListener("resize", updateMode);
+
+function applyPhoneMode() {
+    hideFrame("Calendar");
+    hideFrame("GoogleSheet");
+    hideFrame("Pastebox");
+    hideFrame("Weather");
+    hideFrame("Clock");
+    hideFrame("Toolbox");
+
+    setMsgHeaderPhone();
+    setYTPlaylistPhone();
+    setJaseworld2Phone();
+    setBackgroundPhone();
+}
+
+function applyDesktopMode() {
+    showFrame("Calendar");
+    showFrame("GoogleSheet");
+    showFrame("Pastebox");
+    showFrame("Weather");
+    showFrame("Clock");
+    showFrame("Toolbox");
+
+    setMsgHeaderDesktop();
+    setYTPlaylistDesktop();
+    setJaseworld2Desktop();
+    setBackgroundDesktop();
+}
+
+function hideFrame(id) {
+    const el = document.getElementById(id);
+    if (el) el.style.display = "none";
+}
+
+function showFrame(id) {
+    const el = document.getElementById(id);
+    if (el) el.style.display = "block";
+}
