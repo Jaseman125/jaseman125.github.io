234b26cb95d056261c87ee8a79dafbbde461d583
Jaseman <jaseman125@googlemail.com>
2025-12-27 04:04:52 +0000

    updated

--- index.js
+++ index.js
@@ -12,29 +12,29 @@ window.addEventListener("load", updateMode);
 window.addEventListener("resize", updateMode);
 
 function applyPhoneMode() {
+    // Hide ALL frames in phone mode
+    hideFrame("IF01");
+    hideFrame("IF02");
+    hideFrame("IF03");
+    hideFrame("IF04");
     hideFrame("IF05");
     hideFrame("IF06");
     hideFrame("IF07");
     hideFrame("IF08");
     hideFrame("IF09");
     hideFrame("IF10");
-
-    setMsgHeaderPhone();
-    setYTPlaylistPhone();
-    setJaseworld2Phone();
-    setBackgroundPhone();
 }
 
 function applyDesktopMode() {
+    // Show all frames EXCEPT IF08 + IF09 (toolbox controls those)
+    showFrame("IF01");
+    showFrame("IF02");
+    showFrame("IF03");
+    showFrame("IF04");
     showFrame("IF05");
     showFrame("IF06");
     showFrame("IF07");
     showFrame("IF10");
-
-    setMsgHeaderDesktop();
-    setYTPlaylistDesktop();
-    setJaseworld2Desktop();
-    setBackgroundDesktop();
 }
 
 function hideFrame(id) {
@@ -45,22 +45,4 @@ function hideFrame(id) {
 function showFrame(id) {
     const el = document.getElementById(id);
     if (el) el.style.display = "block";
-}
-
-function setBackgroundPhone() {
-    const el = document.getElementById("IF01");
-    el.style.width = "100vw";
-    el.style.height = "100vh";
-    el.style.left = "0px";
-    el.style.top = "0px";
-}
-
-function setYTPlaylistPhone() {
-    const el = document.getElementById("IF03");
-    el.style.width = "288px";
-}
-
-function setYTPlaylistDesktop() {
-    const el = document.getElementById("IF03");
-    el.style.width = "960px";
 }
