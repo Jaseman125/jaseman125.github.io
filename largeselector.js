dbf2691c35b9a54dda89df5c7b98cbaf802fc0f9
Jaseman <jaseman125@googlemail.com>
2025-12-29 09:18:41 +0000

    no looping slides

--- largeselector.js
+++ largeselector.js
@@ -8,22 +8,18 @@ function updateContent() {
 function nextPage() {
     if (currentPage < pages.length - 1) {
         currentPage++;
-    } else {
-        currentPage = 0; // wrap around
+        updateContent();
     }
-    updateContent();
 }
 
 function prevPage() {
     if (currentPage > 0) {
         currentPage--;
-    } else {
-        currentPage = pages.length - 1; // wrap around
+        updateContent();
     }
-    updateContent();
 }
 
 document.getElementById("ls-left").addEventListener("click", prevPage);
 document.getElementById("ls-right").addEventListener("click", nextPage);
 
-updateContent(); // initial load
+updateContent();
